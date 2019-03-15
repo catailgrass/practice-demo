e648bdff380b391e68905f39edc4ae7a48785e5e
flycat <flycat@jgame.onaliyun.com>
2019-03-15 18:48:23 +0800

    线程并发 : 不知具体执行业务对象 及其个数

---
+++ webdemo2/practice/src/main/java/practice/thread/business_to_class/CatDoJob.java
@@ -0,0 +1,16 @@
+package practice.thread.business_to_class;
+
+/**
+ * @Author Gramo
+ * @Time 2019/3/15
+ * @DES
+ */
+public class CatDoJob extends AnimalJob{
+    public CatDoJob() {
+    }
+
+    @Override
+    public String execute() {
+        return "I eat and sleep";
+    }
+}
