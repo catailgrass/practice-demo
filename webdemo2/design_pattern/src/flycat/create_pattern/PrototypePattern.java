88bc0f2a32759e4936fdc7a726a788785533ca9a
flycat <catailgrass@163.com>
2019-05-30 09:38:02 +0800

    创建模式

--- webdemo2/design_pattern/src/flycat/create_pattern/PrototypePattern.java
+++ webdemo2/design_pattern/src/flycat/create_pattern/PrototypePattern.java
@@ -1,8 +1,19 @@
 package flycat.create_pattern;
 
+/**
+ * 原型模式定义: 用原型实例指定创建对象的种类,并且通过拷贝这些原型创建新的对象.
+ * Prototype模式允许一个对象再创建另外一个可定制的对象，
+ * 根本无需知道任何如何创建的细节,工作原理是:通过将一个原型对象传给那个要发动创建的对象，
+ * 这个要发动创建的对象通过请求原型对象拷贝它们自己来实施创建。
+ */
 public class PrototypePattern {
 
+
     public static class AbstractSpoon implements Cloneable{
+
+        public AbstractSpoon(String spoonName) {
+            this.spoonName = spoonName;
+        }
 
         private String spoonName;
 
@@ -23,7 +34,7 @@ public class PrototypePattern {
     public static class SoupSpoon extends AbstractSpoon{
 
         public SoupSpoon(String name) {
-            this.setSpoonName(name);
+            super(name);
         }
     }
 
